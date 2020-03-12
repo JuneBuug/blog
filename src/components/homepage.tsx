47009df27971fd4a0571deb1715673661a55a778
june-y-kim <june.y.kim@navercorp.com>
2020-03-12 16:38:09 +0900

    이미지 파일들 크기를 조정하라

--- src/components/homepage.tsx
+++ src/components/homepage.tsx
@@ -29,6 +29,7 @@ const Homepage = ({ posts }: PostsProps) => {
   console.log(p)
   return (
     <Layout sx={{mt: 0}}>
+      <img src="wiki.png" width="100%" />
       <Title text="WIKI: 최근 변경된 문서">
         <Link to={replaceSlashes(`/${basePath}/${blogPath}`)}>블로그로 보기</Link>
       </Title>
