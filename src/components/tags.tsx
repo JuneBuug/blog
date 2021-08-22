1d2069cf034c2316fec06a0b4c3f3aaa7560a9db
junebuug <junee613@gmail.com>
2021-08-22 09:47:42 +0900

    tags component만 롤백

--- src/components/tags.tsx
+++ src/components/tags.tsx
@@ -1,12 +1,12 @@
 /** @jsx jsx */
-import { jsx, Link as TLink, Heading, Box, Flex } from "theme-ui"
+import { jsx, Styled } from "theme-ui"
+import { Box, Flex } from "@theme-ui/components"
 import kebabCase from "lodash.kebabcase"
 import { Link } from "gatsby"
 import Layout from "./layout"
 import useMinimalBlogConfig from "../hooks/use-minimal-blog-config"
-import Seo from "./seo"
+import SEO from "./seo"
 import replaceSlashes from "../utils/replaceSlashes"
-
 
 type PostsProps = {
   list: {
@@ -17,26 +17,24 @@ type PostsProps = {
 
 const Tags = ({ list }: PostsProps) => {
   const { tagsPath, basePath } = useMinimalBlogConfig()
-
+  console.log(list)
   return (
     <Layout>
-      <Seo title="Tags" />
-      <Heading as="h1" variant="styles.h1">
-        Tags
-      </Heading>
-      <Box mt={[4, 5]}>
-        {/* {list.map((listItem) => (
+      <SEO title="Tags" />
+      <Styled.h2>Tags</Styled.h2>
+      {/* <Box mt={[4, 5]}>
+        {list.map(listItem => (
           <Flex key={listItem.fieldValue} mb={[1, 1, 2]} sx={{ alignItems: `center` }}>
-            <TLink
+            <Styled.a
               as={Link}
               sx={{ variant: `links.listItem`, mr: 2 }}
               to={replaceSlashes(`/${basePath}/${tagsPath}/${kebabCase(listItem.fieldValue)}`)}
             >
               {listItem.fieldValue} <span sx={{ color: `secondary` }}>({listItem.totalCount})</span>
-            </TLink>
+            </Styled.a>
           </Flex>
-        ))} */}
-      </Box>
+        ))}
+      </Box> */}
     </Layout>
   )
 }
